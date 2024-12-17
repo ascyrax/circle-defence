7d85fd3cfd65c3308660d1dc0660fd0e69a90b2c
ascyrax <ascyrax01@gmail.com>
2024-12-17 14:27:22 +0530

    render upgrade node + fixed z-index of nodes wrt enemies

--- gamePlay.gd
+++ gamePlay.gd
@@ -2,7 +2,7 @@ extends Control
 
 @export var enemyScene: PackedScene  # Drag your Enemy scene here
 @export var shooterScene: PackedScene
-var enemySpawnInterval: float = 1
+var enemySpawnInterval: float = 0.1
 var shooterContainerPosition
 var shooterGlobalPosition
 var shooterSpriteSize = Vector2(0.0, 0.0)
@@ -10,10 +10,13 @@ var shooterSpriteSize = Vector2(0.0, 0.0)
 func _ready() -> void:
 	# Defer the size calculation to ensure layout is finalized
 	call_deferred("_spawn_shooter")
-	#call_deferred("_set_shooter_range_scale")
+	call_deferred("_set_shooter_range_scale")
 	call_deferred("spawn_enemies")
 	call_deferred("_render_upgrade_window")
 
+func _process(delta: float) -> void:
+	pass
+	
 func _spawn_shooter():
 	var shooterInstance = shooterScene.instantiate() as Node2D
 	var shooterContainer = $"Panel/VBoxContainer/GamePlayNode" as Control
@@ -35,13 +38,6 @@ func _set_shooter_range_scale():
 	var correctScaleX = (shooterRange.radius *2.0) / (rangeSprite.texture.get_width() * 1.0)
 	var correctScaleY = (shooterRange.radius *2.0) / (rangeSprite.texture.get_height() * 1.0)
 	rangeSprite.scale = Vector2(correctScaleX, correctScaleY)
-	#print(rangeSprite.texture.get_size())
-	#print(shooterRange.radius)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 
 func spawn_enemies():
 	var enemySpawnTimer = Timer.new()
@@ -83,3 +79,16 @@ func _spawn_enemy():
 		var direction = (shooterGlobalPosition - newEnemy.position).normalized()
 		newEnemy.set_direction(direction, shooterGlobalPosition)
 		newEnemy.set_collision_from_shooter(shooterSpriteSize)
+
+func _render_upgrade_window():
+	var attackUpgradeScene = load("res://attack_upgrades.tscn") as PackedScene
+	var attackUpgradeInstance = attackUpgradeScene.instantiate() as Control
+	print(attackUpgradeInstance)
+	var upgradesContainer = $"Panel/VBoxContainer/Upgrades" as Control
+	print(upgradesContainer)
+	upgradesContainer.add_child(attackUpgradeInstance)
+	attackUpgradeInstance.set_anchors_preset(Control.PRESET_VCENTER_WIDE)
+	#attackUpgradeInstance.anchor_top = 0
+	#attackUpgradeInstance.anchor_bottom = 1
+	
+	
