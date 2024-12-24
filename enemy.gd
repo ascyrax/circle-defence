f6c1d8eca5f057e10e71c95843db4414c5fd8953
ascyrax <ascyrax01@gmail.com>
2024-12-24 20:16:23 +0530

    added explosion animation

--- enemy.gd
+++ enemy.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var speed: float = randi_range(50, 150)  # Speed of the enemy
+var _explosionScene = load("res://enemy_explosion.tscn") as PackedScene
 var direction: Vector2 = Vector2.ZERO  # Direction toward the center
 var shooterContainerPosition : Vector2
 var _health: float = 1.0
@@ -62,8 +63,8 @@ func on_bullet_hit(bullet: Area2D):
 		#var bulletDamage = bullet.get_parent().get_current_bullet_damage()
 		_update_health(_bulletDamage)
 		if(_health <= 0.0):
-			var audioNode = $/root/GamePlay/Panel/VBoxContainer/EnemyExplosion as AudioStreamPlayer2D
-			audioNode.play()
+			_play_explosion_animation()
+			_play_explosion_audio()
 			queue_free()
 			GlobalData.update_wave_enemies_killed(+1)
 			update_cash_value()
@@ -87,3 +88,14 @@ func get_current_enemy_health():
 
 func get_current_enemy_damage():
 	return _damage
+
+func _play_explosion_audio():
+	var audioNode = $/root/GamePlay/Panel/VBoxContainer/EnemyExplosion as AudioStreamPlayer2D
+	audioNode.play()
+
+func _play_explosion_animation():
+	var _explosionContainer = $/root/GamePlay/Panel/VBoxContainer/EnemyExplosionSpawner as Node2D
+	var _explosionInstance = _explosionScene.instantiate() as AnimatedSprite2D
+	_explosionInstance.position = position
+	_explosionContainer.add_child(_explosionInstance)
+	_explosionInstance.play("explode01")
---
+++ enemy_explosion.gd
@@ -0,0 +1,16 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	call_deferred("_temp")
+
+func _temp():
+	var _timer = $Timer as Timer
+	_timer.wait_time = 1.0
+	_timer.one_shot = true
+	_timer.connect("timeout", _delete_explosion)
+	_timer.start()
+
+func _delete_explosion():
+	queue_free()
